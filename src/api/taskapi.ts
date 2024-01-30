992c78d7a377ab364d6ab6d22da972847890bb71
Dan Saa <dansaa8@protonmail.com>
2024-01-30 12:03:57 +0100

    Remove superflous variable in each request function

--- src/api/taskapi.ts
+++ src/api/taskapi.ts
@@ -3,7 +3,6 @@ import axios, { AxiosRequestConfig }  from "axios";
 
 const getAxiosConfig = (): AxiosRequestConfig => {
   const token = sessionStorage.getItem("jwt");
-  
   return {
     headers: {
       'Authorization': token,
@@ -15,25 +14,21 @@ const getAxiosConfig = (): AxiosRequestConfig => {
 const URL = `${import.meta.env.VITE_API_URL}/api/tasks`;
 
 export const getTasks = async (): Promise<TaskResponse[]> => {
-  const token = sessionStorage.getItem("jwt");
   const response = await axios.get(URL, getAxiosConfig());
   return response.data as TaskResponse[];
 };
 
 export const deleteTask = async (id: number): Promise<TaskResponse> => {
-  const token = sessionStorage.getItem("jwt");
   const response = await axios.delete(URL + `/${id}`, getAxiosConfig());
   return response.data;
 };
 
 export const addTask = async (task: Task): Promise<TaskResponse> => {
-  const token = sessionStorage.getItem("jwt");
   const response = await axios.post(URL, task, getAxiosConfig());
   return response.data;
 };
 
 export const updateTask = async (taskEntry: TaskEntry): Promise<TaskResponse> => {
-  const token = sessionStorage.getItem("jwt");
   const response = await axios.put(URL + `/${taskEntry.id}`, taskEntry.task, getAxiosConfig());
   return response.data;
 };
