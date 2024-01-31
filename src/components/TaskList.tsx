76e2b7b33c2bfa91292bcf341eb28152c02fc8c4
Dan Saa <dansaa8@protonmail.com>
2024-01-31 01:16:53 +0100

    Add some help comments for exam

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,18 +15,22 @@ type TaskListProps = {
 }
 
 function TaskList({ logOut }: TaskListProps) {
-  const [open, setOpen] = useState(false);
+  const [snackOpen, setSnackOpen] = useState(false);
   const queryClient = useQueryClient();
 
-    const { data, error, isSuccess } = useQuery({
-    queryKey: ["tasks"],
-    queryFn: getTasks,
+// data håller resultatet från queryn
+// error innehåller något eventuellt error som uppstår under query exekvering
+// isSuccess är en boolean som indikerar om queryn gick igenom eller inte
+    const { data, error, isSuccess } = useQuery({ // hook som används för att fånga o hantera data från asynkron data källa.
+    queryKey: ["tasks"], // Innehåller en unik identifierar för queryn. Sätts till "tasks". Används internt av react-query för att hantera & cacha queries
+    queryFn: getTasks, // specifierar en function som definerar hur data hämtas för queryn.
   });
-  
-  const { mutate } = useMutation(deleteTask, {
-    onSuccess: () => {
-      setOpen(true);
-      queryClient.invalidateQueries({ queryKey: ["tasks"] });
+
+// deleteTask kallas på när mutate invokas
+  const { mutate } = useMutation(deleteTask, { // Hook som används för att göra asynkrona mutationrequests(POST, PUT, DELETE osv..) o hantera state för mutationen
+    onSuccess: () => { 
+      setSnackOpen(true);
+      queryClient.invalidateQueries({ queryKey: ["tasks"] });// Invaliderar queryn med key "tasks" vilket triggar en refetch av tasks.
     },
     onError: (err) => {
       console.log(err);
@@ -42,18 +46,19 @@ function TaskList({ logOut }: TaskListProps) {
       sortable: false,
       filterable: false,
       disableColumnMenu: true,
-      renderCell: (params: GridCellParams) => (
-        <EditTask taskdata={params.row} />
+      renderCell: (params: GridCellParams) => ( // Här defineras cell content logiken. Funktion som tar ett params argument, som innehåller info om nuvarnade cell
+        <EditTask taskdata={params.row} /> // Skickar hela params.row objektet som en prop som heter taskdata till EditTask componenten.
+        // EditTask tar emot hela data objektet som motsvarar raden som cellen är i.
       ),
     },
     {
       field: "delete",
-      headerName: "",
+      headerName: "",  
       width: 90,
       sortable: false,
       filterable: false,
       disableColumnMenu: true,
-      renderCell: (params: GridCellParams) => (
+      renderCell: (params: GridCellParams) => ( 
         <IconButton
           aria-label="delete"
           size="small"
@@ -85,16 +90,16 @@ function TaskList({ logOut }: TaskListProps) {
         <AddTask />
         <Button onClick={logOut}>Log out</Button>
       </Stack>
-        <DataGrid
-          rows={data}
-          columns={columns}
-          disableRowSelectionOnClick={true}
-          getRowId={(row) => row.id}
+        <DataGrid // tabell-liknande component för att visa data i gridformat
+          rows={data} // varje objekt i data arrayen representerar en rad i griden
+          columns={columns} // specificerar kolumnerna som ska visas i datagriden. 
+          disableRowSelectionOnClick={true} // disablar rad selection när en rad klickas på.
+          getRowId={(row) => row.id} // Denna prop specificerar hur en unik identiferare för varje rad i DataGrid.
         />
         <Snackbar
-          open={open}
+          open={snackOpen}
           autoHideDuration={2000}
-          onClose={() => setOpen(false)}
+          onClose={() => setSnackOpen(false)}
           message="Task deleted"
         />
       </>
